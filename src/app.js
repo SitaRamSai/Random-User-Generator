0d9e6b0f94eb0b2fc90976d9069a398794b8b498
SitaRamSai <57385355+SitaRamSai@users.noreply.github.com>
2021-01-28 22:05:46 -0600

    Initial push

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import store from './store/store';
+import { addUsers } from './actions/users';
+import Header from './components/Header';
+import UsersList from './components/UserList';
+import { Provider } from 'react-redux';
+import './css/styles.css';
+
+
+
+
+class App extends React.Component {
+    componentDidMount() {
+        axios.get('/users')
+        .then(response => {
+           console.log(response.data);
+           store.dispatch(addUsers(response.data.results));
+
+        })
+    }
+    render() {
+        return (
+            <div className="main-section">
+             <Header />
+             <UsersList />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+     <App />
+    </Provider>,
+   document.getElementById('root'));
